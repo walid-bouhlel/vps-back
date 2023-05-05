30662338f87012f252cca138f1f01b304351ca58
Walid BOUHLEL <walid@bouhlel.tn>
2023-05-05 14:03:07 +0100

    Working on authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -11,14 +16,27 @@ class AuthController extends Controller
     use HttpResponses;
 
 
-    public function login()
+    public function login(LoginUserRequest $request)
     {
-        return 'this is my login method';
+        $request->validated($request->all());
+        if(!Auth::attempt([$request->only('email','password')])){
+            return $this->error('','Credentials do not match',401);
+        }
     }
 
-    public function register()
-    {
-        return response()->json('this is my register method');
+    public function register(StoreUserRequest $request)
+    {   
+        $request->validated($request->all());
+        $user=User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $this->succes([
+            'user' => $user,
+            'token' => $user-> createToken('API token of'. $user->name)->plainTextToken
+        ]);
     }
 
     public function logout()
