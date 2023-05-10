c2827e010c8f560e6f32ecd9c0c5f48b2a3a60d5
Walid BOUHLEL <walid@bouhlel.tn>
2023-05-10 18:00:16 +0100

    test

--- routes/api.php
+++ routes/api.php
@@ -20,18 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
 //public
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
-
-
 //protected
-
-
 Route::group(['middleware'=>['auth:sanctum']], function() {
     Route::post('/logout',[AuthController::class,'logout']);
     Route::resource('/vps',VpsController::class);
-
 });
